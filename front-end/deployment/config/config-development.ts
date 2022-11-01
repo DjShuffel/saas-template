738b648e2f17c2f820df21e9b0826b6096df6eaf
DjShuffel <aaron.rennau@gmx.de>
2022-11-01 23:29:04 +0100

    new file:   .DS_Store 	new file:   back-end/.DS_Store 	new file:   back-end/Pulumi.dev.yaml 	new file:   back-end/package-lock.json 	new file:   front-end/.graphqlconfig.yml 	new file:   front-end/amplify/.config/project-config.json 	new file:   front-end/amplify/README.md 	new file:   front-end/amplify/backend/backend-config.json 	new file:   front-end/amplify/backend/tags.json 	new file:   front-end/amplify/backend/types/amplify-dependent-resources-ref.d.ts 	new file:   front-end/amplify/cli.json 	new file:   front-end/amplify/hooks/README.md 	new file:   front-end/amplify/team-provider-info.json 	modified:   front-end/deployment/config/config-development.ts 	modified:   front-end/graphql/API.ts 	new file:   front-end/package-lock.json 	new file:   front-end/src/graphql/schema.json

--- front-end/deployment/config/config-development.ts
+++ front-end/deployment/config/config-development.ts
@@ -14,10 +14,10 @@ const configDevelopment = {
   /**
    * Add the details from the Pulumi output here, after running 'pulumi up'
    */
-  USER_POOL_CLIENT_ID: '',
-  USER_POOL_ID: '',
-  IDENTITY_POOL_ID: '',
-  GRAPHQL_ENDPOINT: '',
+  USER_POOL_CLIENT_ID: '3a7toius4jsft0ipf8969pp8tu',
+  USER_POOL_ID: 'eu-central-1_Pt6qJhH3T',
+  IDENTITY_POOL_ID: 'eu-central-1:b4e2c1e0-a5c8-4768-9c92-969b0e107bdc',
+  GRAPHQL_ENDPOINT: 'https://sx6iaujd6jc6dfxdegex24ujrq.appsync-api.eu-central-1.amazonaws.com/graphql',
 };
 
 export default configDevelopment;
